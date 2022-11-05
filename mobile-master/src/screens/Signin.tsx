f9f8109f3eea9a2bf1040ff188fca3357160b2b6
GadiegoN <ngadiego@gmail.com>
2022-11-05 12:11:30 -0300

    project finally nlw-copa

---
+++ mobile-master/src/screens/Signin.tsx
@@ -0,0 +1,36 @@
+import { Center, Icon, Text } from "native-base";
+import Logo from '../assets/logo.svg';
+import { Fontisto } from '@expo/vector-icons';
+import { useAuth } from '../hooks/useAuth'
+import { Button } from "../components/Button";
+
+export function SignIn() {
+    const { signIn, isUserLoding } = useAuth();
+
+    // console.log('Dados do usuário => ', user)
+
+    return (
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40} />
+            <Button
+                title="ENTRAR COM O GOOGLE"
+                type="SECONDARY"
+                leftIcon={
+                    <Icon 
+                        as={Fontisto}
+                        name="google" 
+                        color="white" 
+                        size="md"
+                    />
+                }
+                mt={12}
+                onPress={signIn}
+                isLoading={isUserLoding}
+                _loading={{ _spinner: {color: 'white'}}}
+            />
+            <Text color="white" textAlign="center" mt={4}>
+                Não utilizamos nenhuma informação além {'\n'} de seu e-mail para criação de sua conta.
+            </Text>
+        </Center>
+    )
+}
